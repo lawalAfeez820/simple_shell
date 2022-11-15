479dbdbb570975a6f8f9c02401cbd5d202766115
lawalafeez820 <lawalafeez820@gmail.com>
2022-11-14 22:34:39 -0800

    update

--- shell.h
+++ shell.h
@@ -3,13 +3,14 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>                               
 #include <unistd.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
 #include <limits.h>
-#include <fcntl.h>
+
 #include <errno.h>
 
 /* for read/write buffers */
